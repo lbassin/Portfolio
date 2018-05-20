772f0c6f5e8a47e52c9f51df1f8ce0eca8704ff4
Laurent Bassin <laurent@bassin.info>
2018-05-20 16:53:09 +0200

    Rename project preview image to thumbnail

--- src/app/models/IProject.ts
+++ src/app/models/IProject.ts
@@ -1,9 +1,13 @@
 export interface IProject {
   name: string;
-  image: string;
+  thumbnail: string;
   caption: string;
   links: Array<{
     label: string;
     url: string;
+  }>;
+  images: Array<{
+    path: string;
+    caption: string;
   }>;
 }
