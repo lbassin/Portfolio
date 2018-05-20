9846a9f776f883463817ca8ed366f513c26047e0
Laurent Bassin <laurent@bassin.info>
2018-05-20 21:30:54 +0200

    Zoom on project images

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, Renderer2, ViewChild } from '@angular/core';
 import { CardAnimations } from './card.animation';
 import { IProject } from '../../models/IProject';
 
@@ -13,6 +13,41 @@ export class CardComponent {
   @Input() expanded: boolean;
   @Input() blur = true;
   @Input() project: IProject;
-  @Input() toggleFunction;
+
+  @ViewChild('cardDetail') cardDetail: ElementRef;
+
+  constructor(private renderer: Renderer2) {
+  }
+
+  zoomImage(event): void {
+    const source = event.target;
+    const sourcePosition = source.getBoundingClientRect();
+
+    const image: HTMLImageElement = this.renderer.createElement('img');
+    image.src = source.src;
+
+    this.renderer.addClass(image, 'zoomed');
+    this.renderer.setStyle(image, 'top', sourcePosition.top + 'px');
+    this.renderer.setStyle(image, 'left', sourcePosition.left + 'px');
+    this.renderer.setStyle(image, 'width', sourcePosition.width + 'px');
+
+    this.renderer.appendChild(this.cardDetail.nativeElement, image);
+    this.renderer.addClass(source, 'hide');
+
+    setTimeout(() => {
+      this.renderer.addClass(image, 'move');
+
+      const closeListener = this.renderer.listen('body', 'click', () => {
+        this.renderer.removeClass(image, 'move');
+        closeListener(); // Remove listener
+
+        setTimeout(() => {
+          this.renderer.removeChild(this.cardDetail.nativeElement, image);
+          this.renderer.removeClass(source, 'hide');
+        }, 350);
+      });
+
+    }, 1);
+  }
 
 }
